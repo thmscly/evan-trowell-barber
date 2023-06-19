7a78320833b9f1cca7da1e73cb91d67c9f655099
T.C. Cox <thmscly@gmail.com>
2023-06-19 13:26:49 -0400

    styled About

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="about">
     <h1>*Barbershop*</h1>
-    <h2>Classic Hair for the Modern Man</h2>
+    <div class="about-subsection">
+      <h2>Evan Trowell started *Barbershop* with nothing but scissors, know-how, and a vision.</h2>
+      <h2>
+        Now, with 6 experienced and licensed barbers under his wing, Evan's taken his vision to a
+        whole new level.
+      </h2>
+      <h2>
+        Evan and his team are offering the best men's haircuts, women's haircuts, and hair styling
+        for all in the greater Jacksonville, FL area.
+      </h2>
+    </div>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
     display: flex;
+    flex-direction: column;
     align-items: center;
+    row-gap: 3rem;
+  }
+  .about-subsection {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    row-gap: 2rem;
   }
 }
 </style>
